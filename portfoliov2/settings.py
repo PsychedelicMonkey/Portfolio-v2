b0ed71efce023ba1bc5442c51c051ead695c96d4
PsychedelicMonkey <ryan.getz.96@gmail.com>
2021-08-23 22:56:51 -0600

    Environment variables

--- portfoliov2/settings.py
+++ portfoliov2/settings.py
@@ -11,20 +11,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3g=zge^(nfkd7)*-d!e9)w0sx))r=p9jq1o%&5ko$9jyvlz!6e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
